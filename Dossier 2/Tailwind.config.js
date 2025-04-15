957f57d0a5039f808e063d2a84153ca6cb45d146
Bredou kouadio <jhonskyload@gmail.com>
2025-04-15 13:00:37 +0000

    Create Tailwind.config.js

---
+++ Dossier 2/Tailwind.config.js
@@ -0,0 +1,18 @@
+// Fichier : src/pages/Profile.jsx (affichage/modification du profil utilisateur avec champs : prénom, nom, email, langue, etc.)
+
+// Fichier : tailwind.config.js
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// Fichier : postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
